3980a051a0b57b2256a98982fb7d00767bee07a0
kinnerayashwanthsai-cell <kinnerayashwanthsai@gmail.com>
2026-09-24 22:53:45 +0530

    Add files via upload

---
+++ ARTS.C
@@ -0,0 +1,21 @@
+//program to calculate arthematic operators.
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+   int x=54;
+   clrscr();
+   x*=05;
+   printf("x=%d\n",x);
+   x+=22;
+   printf("x=%d\n",x);
+   x-=30;
+   printf("x=%d\n",x);
+   x/=02;
+   printf("x=%d\n",x);
+   x%=04;
+   printf("x=%d",x);
+   getch();
+}
